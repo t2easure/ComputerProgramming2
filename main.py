3ec7dc056da22b517d4be607561ef32039d5ffca
t2easure <tjduswl8@gmail.com>
2024-11-28 16:39:10 +0900

    new

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ def main():
 
     subjects = module_a.subject_and_type(dataframe)
     selected_subject = module_a.selectsubject(subjects)
-    types = module_a.typefromsubject(dataframe, selected_subject)
-    selected_type = module_a.type(types) if len(types) > 0 else None
-    
-    module_b.module_b(dataframe, selected_subject, selected_type)
+    selected_subject_index = subjects.tolist().index(selected_subject) + 1
+
+    if selected_subject_index in [1, 2]:
+        module_b.module_b(dataframe, selected_subject)
+    else:
+        types = module_a.typefromsubject(dataframe, selected_subject)
+        selected_type = module_a.type(types) if len(types) > 0 else None
+        module_b.module_b(dataframe, selected_subject, selected_type)
 
 if __name__ == "__main__":
     main()
