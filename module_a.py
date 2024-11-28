8eba5db0be541ca92b25789cb5eeb9fcc09054cf
t2easure <tjduswl8@gmail.com>
2024-11-28 16:27:59 +0900

    new

--- module_a.py
+++ module_a.py
@@ -16,16 +16,16 @@ def typefromsubject(dataframe, subject):
 
 
 def selectsubject(subjects):
-    print("과목 목록:")
+    print("\n과목 목록:\n")
     for i, subject in enumerate(subjects):
         print(f"{i + 1}. {subject}")
-    selected_index = int(input("원하는 과목의 번호를 작성하세요: ")) - 1
+    selected_index = int(input("\n원하는 과목의 번호를 작성하세요: ")) - 1
     return subjects[selected_index]
 
 
 def type(types):
-    print("유형 목록:")
+    print("\n유형 목록:")
     for i, type_ in enumerate(types):
         print(f"{i + 1}. {type_}")
-    selected_index = int(input("원하는 유형의 번호를 선택하세요: ")) - 1
+    selected_index = int(input("\n원하는 유형의 번호를 선택하세요: ")) - 1
     return types[selected_index]
